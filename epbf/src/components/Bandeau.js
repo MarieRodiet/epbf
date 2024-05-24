f7308bf14673bb44a0adb6910b1cd97528a22276
mmoore <marie.moore@mgas.fr>
2024-05-24 16:51:24 +0200

    fix bandeau style

--- epbf/src/components/Bandeau.js
+++ epbf/src/components/Bandeau.js
@@ -1,11 +1,12 @@
 const Bandeau = ({ pictureFile, titre }) => {
 	return (
 		<div className="bandeau">
-			<img className="bandeau-img"
+			<img
+				className="bandeau-img"
 				src={pictureFile}
 				alt={titre}
 			/>
-            <p>{titre}</p>
+			<p className="bandeau-titre">{titre}</p>
 		</div>
 	);
 };
