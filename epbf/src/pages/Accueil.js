1d04ffd5788e0f9b853c0b9dc4099e7e337e28d5
mmoore <marie.moore@mgas.fr>
2024-09-03 14:26:45 +0200

    add content

--- epbf/src/pages/Accueil.js
+++ epbf/src/pages/Accueil.js
@@ -1,6 +1,7 @@
 import Bandeau from '../components/Bandeau';
 import bandeauImg from '../assets/img/bandeau/jesuisnouveau.jpg';
-
+import togetherImg from '../assets/img/together.png';
+import { versetAccueil } from '../data/texte_accueil';
 const Accueil = () => {
 	const titre = 'Accueil';
 	return (
@@ -8,6 +9,12 @@ const Accueil = () => {
 			<Bandeau
 				pictureFile={bandeauImg}
 				titre={titre}
+			/>
+			<p>{versetAccueil}</p>
+			<h1>Notre vision</h1>
+			<img
+				src={togetherImg}
+				alt="communauté"
 			/>
 		</div>
 	);
